3846e82d2d5012058eaaf4be5e86e3f316704423
Leo Sanitt <leo@sanitt.com>
2024-06-23 13:50:50 +0100

    adding model

--- middle_end.py
+++ middle_end.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import json
 
+class UserCreate(BaseModel):
+    username: str
+
 app = FastAPI()
 def get_summary(case_data):
     return "SUMMARY"
@@ -16,7 +19,7 @@ def get_similar(case_data):
 
 
 @app.post("/req/")
-async def request(case_data):
+async def request(case_data: UserCreate):
     summary = get_summary(case_data)
     similar_case = get_similar(case_data)
     return {"summary": summary, "similar": similar_case}
