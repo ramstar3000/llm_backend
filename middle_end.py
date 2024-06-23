fd26d24e7a29ef37b7e492b264d71d99eddf74e0
Leo Sanitt <leo@sanitt.com>
2024-06-23 13:26:35 +0100

    changing api to match what we want

--- middle_end.py
+++ middle_end.py
@@ -2,17 +2,23 @@ from fastapi import FastAPI
 import json
 
 app = FastAPI()
-def get_response(case_data):
-    pass
+def get_summary(case_data):
+    return "SUMMARY"
 
 @app.get("/test")
 async def test_route():
     return {"message": "Hey the server seems to be working!"}
 
+
+def get_similar(case_data):
+    return "SIMILAR CASES"
+
+
 @app.get("/req")
 async def request(case_data):
-    response = get_response(case_data)
-    return {"response": response}
+    summary = get_summary(case_data)
+    similar_case = get_similar(case_data)
+    return {"summary": summary, "similar": similar_case}
 
 
 # fastapi dev middle_end.py
