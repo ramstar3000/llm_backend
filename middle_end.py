18dc2fda4cda2fd1e1d8ef63598bda83307a36e2
Leo Sanitt <leo@sanitt.com>
2024-06-23 12:57:28 +0100

    adding frontend interaction to fastapi

--- middle_end.py
+++ middle_end.py
@@ -2,9 +2,17 @@ from fastapi import FastAPI
 import json
 
 app = FastAPI()
+def get_response(case_data):
+    pass
 
 @app.get("/test")
 async def test_route():
     return {"message": "Hey the server seems to be working!"}
+
+@app.get("/req")
+async def request(case_data):
+    response = get_response(case_data)
+    return {"response": response}
+
 
 # fastapi dev middle_end.py
